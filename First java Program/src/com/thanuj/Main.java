a39e0052c6c9ed542afb144fcaecdd45b3c43f13
ThanujGoogli <thanujgoogli@gmail.com>
2025-09-07 20:54:06 +0530

    First program

---
+++ First java Program/src/com/thanuj/Main.java
@@ -0,0 +1,11 @@
+package com.thanuj;  //package is a folder
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+    }
+}
